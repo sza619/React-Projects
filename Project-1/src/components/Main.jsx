e22eee5f2d046a11c49183e445c7cfa4f6d7e64a
Syed Zainul Abidin <73888063+sza619@users.noreply.github.com>
2023-04-26 16:33:43 +0530

    Update Main.jsx

--- Project-1/src/components/Main.jsx
+++ Project-1/src/components/Main.jsx
@@ -8,7 +8,7 @@ export default function Main() {
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has over 200K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
+                <li>Is maintained by Meta</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </main>
