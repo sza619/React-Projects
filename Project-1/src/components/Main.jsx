e4de05d0f5916248bdc88bf6d588e810b863b240
Syed Zainul Abidin <73888063+sza619@users.noreply.github.com>
2023-04-26 16:32:00 +0530

    Create Main.jsx

---
+++ Project-1/src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className="content">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has over 200K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
