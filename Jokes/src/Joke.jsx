87b7473e05f0046124478d82ae2573db545a2b7d
Syed Zainul Abidin <73888063+sza619@users.noreply.github.com>
2023-05-02 18:35:27 +0530

    Update Joke.jsx

--- Jokes/src/Joke.jsx
+++ Jokes/src/Joke.jsx
@@ -1,10 +1,16 @@
 import React from "react"
 
 export default function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
+    function toggleShown(){
+        setIsShown(prevShown => !prevShown)
+    }
+
     return (
         <div>
             {props.setup && <h3>Setup: {props.setup}</h3>}
-            <h4>Punchline: {props.punchline}</h4>
+            {isShown && <h4>Punchline: {props.punchline}</h4>}
+            <button className="btn btn-success" onClick={toggleShown}>{isShown ? "Hide" : "Show"} Punchline</button>
             <hr />
         </div>
     )
